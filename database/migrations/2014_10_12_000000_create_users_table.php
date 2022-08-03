37e7a5fa59018e000ab17c0034407493a6e61e24
rajasht <Raja@shorthillstech.com>
2022-08-03 14:00:20 +0530

    modifiedbike id starting from 501

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('user_type');
+            $table->integer('user_type');
             $table->timestamps();
         });
     }
