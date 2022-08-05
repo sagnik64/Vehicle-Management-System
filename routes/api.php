560c71858a88bb9b50d93fb6c8fe1264ef66cfb4
rajasht <Raja@shorthillstech.com>
2022-08-05 09:55:45 +0530

    Added Some Bike Filter Code

--- routes/api.php
+++ routes/api.php
@@ -22,13 +22,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/users',[UserController::class,'store']);
+
 Route::post('/bikes',[BikeController::class,'store']);
+Route::get('allbikes',[BikeController::class,'getAllBikes']);
+Route::get('/bike/{bike_name}',[BikeController::class,'getbyBikeName']);
+Route::get('/bikes/{brand}',[BikeController::class,'getbyBrand']);
+
 Route::post('cars',[CarController::class,'store']);
 Route::get('cars',[CarController::class,'index']);
-
 Route::get('cars/name/{car_name}',[CarController::class,'getByCarName']);
 Route::get('cars/brand/{brand}',[CarController::class,'getByBrand']);
 Route::get('cars/transmission/{transmission}',[CarController::class,'getByTransmission']);
 Route::get('cars/fuel/{fuel_type}',[CarController::class,'getByFuelType']);
-
 Route::get('cars/price/{price_start}/{price_end}',[CarController::class,'getBetweenPrice']);
+
