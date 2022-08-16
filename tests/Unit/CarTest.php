7453a35bbc930da6c98e05f3e4fd9a2f5634fcf4
Sagnik Mandal <Sagnik@shorthillstech.com>
2022-08-16 09:13:16 +0530

    added unit test for Car

---
+++ tests/Unit/CarTest.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Car;
+use Tests\TestCase;
+
+class CarTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_car_duplication()
+    {
+        $car1 = Car::make([
+            "car_name" => "Venue",
+            "price_rs" => 1070000,
+            "brand" => "Hyundai",
+            "model" => "SX",
+            "model_year" => 2022,
+            "colors_available" => 7,
+            "wheel_count" => 4,
+            "fuel_type" => "Petrol",
+            "record_status" => 1,
+            "first_production_year" => 2019,
+            "transmission" => "Manual",
+            "engine_displacement_cc" => 1197,
+            "seating_capacity" => 5,
+            "fuel_tank_capacity_litres" => 45,
+            "body_type" => "SUV",
+            "mileage_kmpl" => 16,
+            "rpm" => 6000,
+            "max_power_bhp" => 82,
+            "max_torque_nm" => 113.8,
+            "length_mm" => 3995,
+            "width_mm" => 1770,
+            "height_mm" => 1617,
+            "wheel_base_mm" => 2500
+        ]);
+        $car2 = Car::make([
+            "car_name" => "Verna",
+            "price_rs" => 1245000,
+            "brand" => "Hyundai",
+            "model" => "SX",
+            "model_year" => 2022,
+            "colors_available" => 6,
+            "wheel_count" => 4,
+            "fuel_type" => "Diesel",
+            "record_status" => 1,
+            "first_production_year" => 2006,
+            "transmission" => "Manual",
+            "engine_displacement_cc" => 1493,
+            "seating_capacity" => 5,
+            "fuel_tank_capacity_litres" => 45,
+            "body_type" => "Sedan",
+            "mileage_kmpl" => 25,
+            "rpm" => 4000,
+            "max_power_bhp" => 113,
+            "max_torque_nm" => 250,
+            "length_mm" => 4440,
+            "width_mm" => 1729,
+            "height_mm" => 1475,
+            "wheel_base_mm" => 2600
+        ]);
+
+        $this->assertTrue($car1->car_name != $car2->car_name);
+    }
+
+    public function test_delete_car() {
+        $car = Car::make([
+            "car_name" => "Venue",
+            "price_rs" => 1070000,
+            "brand" => "Hyundai",
+            "model" => "SX",
+            "model_year" => 2022,
+            "colors_available" => 7,
+            "wheel_count" => 4,
+            "fuel_type" => "Petrol",
+            "record_status" => 1,
+            "first_production_year" => 2019,
+            "transmission" => "Manual",
+            "engine_displacement_cc" => 1197,
+            "seating_capacity" => 5,
+            "fuel_tank_capacity_litres" => 45,
+            "body_type" => "SUV",
+            "mileage_kmpl" => 16,
+            "rpm" => 6000,
+            "max_power_bhp" => 82,
+            "max_torque_nm" => 113.8,
+            "length_mm" => 3995,
+            "width_mm" => 1770,
+            "height_mm" => 1617,
+            "wheel_base_mm" => 2500
+        ]);
+        $car = Car::first();
+        if($car) {
+            $car->delete();
+        }
+        $this->assertTrue(true);
+    }
+
+    public function test_stores_new_car() {
+        $response = $this->post('/api/cars',[
+            "car_name" => "Verna",
+            "price_rs" => 1245000,
+            "brand" => "Hyundai",
+            "model" => "SX",
+            "model_year" => 2022,
+            "colors_available" => 6,
+            "wheel_count" => 4,
+            "fuel_type" => "Diesel",
+            "record_status" => 1,
+            "first_production_year" => 2006,
+            "transmission" => "Manual",
+            "engine_displacement_cc" => 1493,
+            "seating_capacity" => 5,
+            "fuel_tank_capacity_litres" => 45,
+            "body_type" => "Sedan",
+            "mileage_kmpl" => 25,
+            "rpm" => 4000,
+            "max_power_bhp" => 113,
+            "max_torque_nm" => 250,
+            "length_mm" => 4440,
+            "width_mm" => 1729,
+            "height_mm" => 1475,
+            "wheel_base_mm" => 2600
+        ]);
+        $response->assertStatus(201); 
+    }
+
+    public function test_database() {
+        $this->assertDatabaseHas('cars',[
+            'car_name' => 'Verna'
+        ]);
+        $this->assertDatabaseMissing('cars',[
+            'car_name' => 'Bike'
+        ]);
+    } 
+}
