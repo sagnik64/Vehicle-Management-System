649c23fd85569d69ff169a63c374f3fa03778ab5
Sagnik Mandal <Sagnik@shorthillstech.com>
2022-09-19 18:08:49 +0530

    updated unit tests for CAR

--- tests/Unit/Controller/CarControllerTest.php
+++ tests/Unit/Controller/CarControllerTest.php
@@ -9,6 +9,67 @@ use Tests\TestCase;
 class CarControllerTest extends TestCase
 {
     use RefreshDatabase;
+
+    private function setUpMockDatabase() {
+        $this->post('/api/cars', [
+            "car_name" => "Verna",
+            "price_rs" => 1245000,
+            "brand" => "Hyundai",
+            "model" => "SX",
+            "model_year" => 2022,
+            "colors_available" => 6,
+            "wheel_count" => 4,
+            "fuel_type" => "Diesel",
+            "record_status" => 1,
+            "first_production_year" => 2006,
+            "transmission" => "Manual",
+            "engine_displacement_cc" => 1493,
+            "seating_capacity" => 5,
+            "fuel_tank_capacity_litres" => 45,
+            "body_type" => "Sedan",
+            "mileage_kmpl" => 25,
+            "rpm" => 4000,
+            "max_power_bhp" => 113,
+            "max_torque_nm" => 250,
+            "length_mm" => 4440,
+            "width_mm" => 1729,
+            "height_mm" => 1475,
+            "wheel_base_mm" => 2600,
+            "vin" => "1ABCD23EFGH456789",
+            "engine_number" => "12ABC34567",
+            "user_id" => 2
+        ])->assertCreated();
+
+        $this->post('/api/cars', [
+            "car_name" => "Venue",
+            "price_rs" => 1070000,
+            "brand" => "Hyundai",
+            "model" => "SX",
+            "model_year" => 2022,
+            "colors_available" => 7,
+            "wheel_count" => 4,
+            "fuel_type" => "Petrol",
+            "record_status" => 1,
+            "first_production_year" => 2019,
+            "transmission" => "Manual",
+            "engine_displacement_cc" => 1197,
+            "seating_capacity" => 5,
+            "fuel_tank_capacity_litres" => 45,
+            "body_type" => "SUV",
+            "mileage_kmpl" => 16,
+            "rpm" => 6000,
+            "max_power_bhp" => 82,
+            "max_torque_nm" => 113.8,
+            "length_mm" => 3995,
+            "width_mm" => 1770,
+            "height_mm" => 1617,
+            "wheel_base_mm" => 2500,
+            "vin" => "1ABCD23EFGH456789",
+            "engine_number" => "12ABC34567",
+            "user_id" => 2
+        ])->assertCreated();
+    }
+
     public function test_car_duplication()
     {
         $car1 = Car::make([
@@ -68,6 +129,50 @@ class CarControllerTest extends TestCase
         ]);
 
         $this->assertTrue($car1->car_name != $car2->car_name);
+    }
+
+    public function test_get_all_cars_route()
+    {
+        
+        $this->setUpMockDatabase();
+        
+        $this->get('api/cars')->assertStatus(200);
+    }
+
+    public function test_get_all_cars_json_count()
+    {
+        $this->setUpMockDatabase();
+        
+        $reponse = $this->get('api/cars')
+        ->assertOk();
+        
+        $reponse->assertJsonCount(4);
+    }
+
+    public function test_get_all_cars_json_structure()
+    {
+        $this->setUpMockDatabase();
+        $this->get('api/cars')
+        ->assertOk()
+        ->assertJsonStructure([
+            'success',
+            'code',
+            'message',
+            'data'
+        ]);
+    }
+
+    public function test_get_all_cars_json_fragment()
+    {
+        
+        $this->setUpMockDatabase();
+        $this->get('api/cars')
+        ->assertOk()
+        ->assertJsonFragment([
+            "success" => "true",
+            "code" => 200,
+            "message" => "Car data found"
+        ]);
     }
 
     public function test_delete_car()
