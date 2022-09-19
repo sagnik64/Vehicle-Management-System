026bd13d93c965ffe1bd13b61b232c304a5202f6
Sagnik Mandal <Sagnik@shorthillstech.com>
2022-09-19 17:42:21 +0530

    added feature tests for CART

--- tests/Feature/CartTest.php
+++ tests/Feature/CartTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Models\Car;
 use Tests\TestCase;
+use App\Models\Cart;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -124,7 +125,25 @@ class CartTest extends TestCase
         ])->assertRedirect('profile/customer');
     }
 
-    public function test_add_to_cart_button_visible_in_view()
+    private function addToCartMock() {
+        $this->call('POST', route('cart.store'), [
+            'user_id' => 1,
+            'vehicle_type_id' => 1,
+            'vehicle_type' => 'car',
+            'status' => 0,
+            '_token' => csrf_token()
+        ])->assertCreated();
+
+        $this->call('POST', route('cart.store'), [
+            'user_id' => 1,
+            'vehicle_type_id' => 2,
+            'vehicle_type' => 'car',
+            'status' => 0,
+            '_token' => csrf_token()
+        ])->assertCreated();
+    }
+
+    public function test_add_to_cart_button_is_visible_in_view()
     {
         $this->setUpDatabase();
         $this->user_login_as_customer_user();
@@ -132,5 +151,53 @@ class CartTest extends TestCase
         $data = compact('cars');
         $view = $this->view('profile/customer',$data);
         $view->assertSee('Add to Cart')->assertDontSee('Remove from Cart');
+    }
+
+    public function test_remove_from_cart_button_is_visible_in_view()
+    {
+        $this->setUpDatabase();
+
+        $this->addToCartMock();
+
+        $this->user_login_as_customer_user();
+
+        
+        $cars = Car::all();
+        $data = compact('cars');
+        $view = $this->view('profile/customer',$data);
+        $view->assertSee('Remove from Cart')->assertDontSee('Add to Cart');
+    }
+    
+    public function test_count_of_all_add_to_cart_buttons_are_according_to_database() {
+        $this->setUpDatabase();
+
+        $this->user_login_as_customer_user();
+
+        $addedToCartCount = Cart::where('status','=',1)->get()->count();
+        $this->assertEquals(0,$addedToCartCount);
+
+    }
+
+    public function test_count_of_all_remove_from_cart_buttons_are_according_to_database() {
+        $this->setUpDatabase();
+
+        //Add two items in cart
+        $this->addToCartMock();
+
+        $this->user_login_as_customer_user();
+
+        $addedToCartCount = Cart::where('status','=',1)->get()->count();
+        $this->assertEquals(2,$addedToCartCount);
+    }
+
+    public function test_my_cart_link_shows_list_of_all_cart_items_of_user() {
+        $this->setUpDatabase();
+
+        //Add two items in cart
+        $this->addToCartMock();
+
+        $this->user_login_as_customer_user();
+
+        $this->get('api/cart/1')->assertOk();
     }
 }
